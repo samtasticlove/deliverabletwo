5216cb028977d1eff239e637c706762d2b33363d
Sam <samantha.j.samson@gmail.com>
2020-09-25 10:45:45 -0400

    first commit for deliverable two

---
+++ script.js
@@ -0,0 +1,17 @@
+prompt
+
+const = 50;
+
+/* let rain = Number;
+
+if (rain >= 20){
+    console.log(const *010)
+}
+
+if (rain <= 10){
+console.log( const * 0.20)
+}
+
+ else{
+    does nothing
+ }  */ 
